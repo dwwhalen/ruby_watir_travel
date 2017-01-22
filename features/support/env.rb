518b7bdc23b328de034e35e2c0b129b6f3b6de73
dwwhalen <dwwhalen@gmail.com>
2017-01-21 20:32:51 -0500

    some updates

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ require 'page-object'
 require 'data_magic'
 require 'rspec/expectations'
 require 'rspec/matchers'
-require 'allure-cucumber'
+# require 'allure-cucumber'
 require 'require_all'
 require 'test/unit/assertions'
 require_rel '../../PageObjects'
@@ -13,11 +13,11 @@ include Test::Unit::Assertions
 DataMagic.yml_directory = 'data'
 
 
-include AllureCucumber::DSL
+# include AllureCucumber::DSL
 
-AllureCucumber.configure do |config|
-  config.output_dir = "allure"
-end
+# AllureCucumber.configure do |config|
+#   config.output_dir = "allure"
+# end
 
 if ENV['HEADLESS']
   require 'headless'
